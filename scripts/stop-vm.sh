5b549d7d69f475754a6db655356ff67a9a81c59f
Mike Sedillo <Mike.Sedillo@intel.com>
2020-09-16 13:36:16 -0700

    Continuing the war against absolute paths

--- scripts/stop-vm.sh
+++ scripts/stop-vm.sh
@@ -5,7 +5,9 @@ if [[ $iskvm -ne 1 ]] && [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-envconfig="/var/vm/scripts/env.sh"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd)"
+envconfig="${DIR}/env.sh"
+
 if [ -f $envconfig ]; then
         source $envconfig
 else
